553ee0e192b00ffa1264f9d27fa1690afff5a9bc
Nir Natan <nirn@wix.com>
2016-01-29 14:02:13 +0200

    simple server

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var request = require('request');
+var cors = require('cors');
+const API_KEY = 'bozeXoDhGzNfn-w5bnRhcL';
+var app = express();
+
+app.use(cors());
+
+app.get('/:event/:value1?/:value2?/:value3?', function(req, res) {
+  var iftttUrl = `https://maker.ifttt.com/trigger/${req.params.event}/with/key/${API_KEY}?value1=${req.params.value1 || ''}&value2=${req.params.value2 || ''}&value3=${req.params.value3 || ''}`;
+  console.log(iftttUrl);
+  request(iftttUrl, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Start listening to IFTTT requests on port 3000!');
+});
