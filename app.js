b58a905c66a70248f05a12fb81675ee0faafa1c2
Nir Natan <nirn@wix.com>
2016-01-31 02:42:48 +0200

    Add facebook authentication and moved the ifttt to an inner path

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var passport = require('passport');
 var request = require('request');
+var facebook = require('./facebook');
 var cors = require('cors');
 var API_KEY = 'bozeXoDhGzNfn-w5bnRhcL';
 var app = express();
 
 app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat', key: 'sid'}));
+
+
 app.get('/', function(req, res) {
   request('https://maker.ifttt.com/trigger/button_clicked/with/key/' + API_KEY, function(error, response, body) {
     res.send(body);
   })
 });
-
-app.get('/:event/:value1?/:value2?/:value3?', function(req, res) {
+app.get('/ifttt/:event/:value1?/:value2?/:value3?', function(req, res) {
   var iftttUrl = 'https://maker.ifttt.com/trigger/' + req.params.event + '/with/key/' + API_KEY + '?value1=' + (req.params.value1 || '') + '&value2=' + (req.params.value2 || '') + '&value3=' + (req.params.value3 || '');
   console.log(iftttUrl);
   request(iftttUrl, function(error, response, body) {
@@ -20,6 +29,8 @@ app.get('/:event/:value1?/:value2?/:value3?', function(req, res) {
     }
   });
 });
+
+facebook.init(app);
 
 app.listen(8080, function() {
   console.log('Start listening to IFTTT requests on port 8080!');
---
+++ configuration/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  facebook_api_key: '105280039542920',
+  facebook_api_secret: 'c096127e0cdc6a83c70fc163287116a5',
+  callback_url: 'http://ifttt-proxy.appspot.com/auth/facebook/callback'
+};
