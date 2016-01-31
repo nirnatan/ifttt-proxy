d8cb1e12b591bceba3f05cc0b659400dc1a862ac
Nir Natan <nirn@wix.com>
2016-01-31 01:59:28 +0000

    add the generic iftt api

--- app.js
+++ app.js
@@ -8,27 +8,17 @@ app.use(cors());
 
 app.get('/', function(req, res) {
 	res.send('init');
-  // request('https://maker.ifttt.com/trigger/button_clicked/with/key/' + API_KEY, function(error, response, body) {
-  //   res.send(body);
-  // })
 });
 
-app.get('/take_photo/:url', function(req, res) {
-    var iftttUrl = 'https://maker.ifttt.com/trigger/take_photo/with/key/' + API_KEY + '?value1=' + req.params.url;
-    request(iftttUrl, function(error, response, body) {
-        res.send(body);
-    });
+app.get('/:event/:value1?/:value2?/:value3?', function(req, res) {
+  var iftttUrl = 'https://maker.ifttt.com/trigger/' + req.params.event + '/with/key/' + API_KEY + '?value1=' + (req.params.value1 || '') + '&value2=' + (req.params.value2 || '') + '&value3=' + (req.params.value3 || '');
+  console.log(iftttUrl);
+  request(iftttUrl, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send(body);
+    }
+  });
 });
-
-// app.get('/:event/:value1?/:value2?/:value3?', function(req, res) {
-//   var iftttUrl = 'https://maker.ifttt.com/trigger/' + req.params.event + '/with/key/' + API_KEY + '?value1=' + (req.params.value1 || '') + '&value2=' + (req.params.value2 || '') + '&value3=' + (req.params.value3 || '');
-//   console.log(iftttUrl);
-//   request(iftttUrl, function(error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       res.send(body);
-//     }
-//   });
-// });
 
 app.listen(8080, function() {
   console.log('Start listening to IFTTT requests on port 8080!');
